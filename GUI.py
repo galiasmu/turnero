23fb864bb8e9ddd4f3a4c701ef114c6b653de14f
galiasmu <galiasmuzi@gmail.com>
2023-04-27 13:04:35 -0300

    ventana ya funcionando

--- GUI.py
+++ GUI.py
@@ -5,10 +5,19 @@ class TurneroGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Turnero")
+        self.label = tk.Label(self.root, text="Por favor seleccione su rubro:")
+        self.label.pack()
+        self.libreria_button = tk.Button(self.root, text="Librería", command=lambda: self.sacar_turno("L"))
+        self.libreria_button.pack()
+        self.imprenta_button = tk.Button(self.root, text="Imprenta", command=lambda: self.sacar_turno("I"))
+        self.imprenta_button.pack()
+        self.fotocopias_button = tk.Button(self.root, text="Fotocopias", command=lambda: self.sacar_turno("F"))
+        self.fotocopias_button.pack()
+        self.ropa_button = tk.Button(self.root, text="Bordado de ropa", command=lambda: self.sacar_turno("R"))
+        self.ropa_button.pack()
+        self.root.mainloop()
 
-
-
- def sacar_turno(self, rubro):
+    def sacar_turno(self, rubro):
         main.ticket(rubro)
 
 if __name__ == "__main__":
