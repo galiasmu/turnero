d3a934ec693a9f797ef607cfc5914872c6157dee
galiasmu <galiasmuzi@gmail.com>
2023-05-10 11:55:56 -0300

    observando como mejorar la interfaz, llamando, y tratando de eliminar turnos seleccionados

--- GUI.py
+++ GUI.py
@@ -9,16 +9,29 @@ class TurneroGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Turnero")
+
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        self.root.geometry(f"{screen_width}x{screen_height}")
+        #Botones
         self.label = tk.Label(self.root, text="Por favor seleccione su rubro:")
-        self.label.pack()
+        self.label.pack(pady=screen_height * 0.1)
+
         self.libreria_button = tk.Button(self.root, text="Librería", command=lambda: self.sacar_turno("L"))
-        self.libreria_button.pack()
+        self.libreria_button.pack(pady=screen_height * 0.1)
+
         self.imprenta_button = tk.Button(self.root, text="Imprenta", command=lambda: self.sacar_turno("I"))
-        self.imprenta_button.pack()
+        self.imprenta_button.pack(pady=screen_height * 0.1)
+
         self.fotocopias_button = tk.Button(self.root, text="Fotocopias", command=lambda: self.sacar_turno("F"))
-        self.fotocopias_button.pack()
+        self.fotocopias_button.pack(pady=screen_height * 0.1)
+
         self.ropa_button = tk.Button(self.root, text="Bordado de ropa", command=lambda: self.sacar_turno("R"))
         self.ropa_button.pack()
+        self.root.bind("<Configure>", self.on_resize)
+
+
         self.root.mainloop()
 
     def sacar_turno(self, rubro):
@@ -37,6 +50,12 @@ class TurneroGUI:
 
     def ver_turnos(self):
         ver_turnos_gui = VerTurnosGUI()
+
+    def on_resize(self, event):
+        # Actualizar tamaño de la ventana y reajustar widgets
+        width = event.width
+        height = event.height
+        self.root.geometry(f"{width}x{height}")
 
 if __name__ == "__main__":
 
--- verTurnos.py
+++ verTurnos.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import tkinter as tk
 import json
 
@@ -12,6 +13,8 @@ class VerTurnosGUI:
 
         self.turnos_listbox = tk.Listbox(self.root)
         self.turnos_listbox.pack(expand=True, fill=tk.BOTH, padx=10)
+        self.llamarTurno_button = tk.Button(self.root, text="Llamar turno", command=lambda: llamar_turno(self))
+        self.llamarTurno_button.pack()
 
         self.cargar_turnos()
 
@@ -28,10 +31,47 @@ class VerTurnosGUI:
                         fecha = turno['fecha']
                         hora = turno['hora']
                         self.turnos_listbox.insert(tk.END, f"Rubro: {rubro} | Numero: {numero} | Fecha: {fecha} | Hora: {hora}")
+                        self.TL = self.turnos_listbox.get(0, tk.END)
+                        if self.TL is None:
+                            popup = tk.Toplevel()
+                            popup.title("Ventana emergente")
+                            popup.geometry("200x200")
+
+                            # Agregar widgets a la ventana emergente
+                            label = tk.Label(popup, text="No se encontraron turnos")
+                            label.pack()
                     except json.JSONDecodeError as e:
                         print(f"Error al decodificar la línea JSON: {linea}. Error: {e}")
         except FileNotFoundError:
             print("El archivo turnos.json no se encuentra.")
+
+def llamar_turno(self):
+    select = self.turnos_listbox.curselection()
+    if select:
+        turno_select = self.turnos_listbox.get(select)
+        popup = tk.Toplevel()
+        popup.title("Llamar Turno")
+        popup.geometry("200x200")
+        label = tk.Label(popup, text=f"Llamando al turno: {turno_select}")
+        label.pack()
+        ## eliminar turno
+        self.turnos_listbox.delete(turno_select)
+        with open('turnos.json', 'r+') as f:
+            turno = json.loads(f)
+            turno.remove(turno_select)
+            f.seek(0)
+            json.dump(turno, f, indent=4)
+            f.truncate()
+
+    else:
+        ## si no se selecciona el turno, casilla de error
+        popup = tk.Toplevel()
+        popup.title("Error")
+        popup.geometry("200x200")
+        label = tk.Label(popup, text="No se ha seleccionado ningún turno")
+        label.pack()
+
+
 
 
 if __name__ == "__main__":
